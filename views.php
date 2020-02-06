c49694dc6687f3ba3db1823afa20573b574b79a9
SAMEER PRAJAPATI <sameercse@cse.iitb.ac.in>
2020-02-06 16:46:45 +0530

    Initial Commit

---
+++ views.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Hello" ;
+$views = 0;
+$visitors_file = 'visitors.txt';
+
+if (file_exists($visitors_file))
+{
+    $views = (int)file_get_contents($visitors_file) ;
+}
+else {
+   die('no post data to process');
+}
+$views++;
+
+file_put_contents($visitors_file, $views);
+?>
